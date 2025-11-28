8e3d460524800baf97843dcef0a8a4059c559687
manjeet-voletic <manjeetvoletic@gmail.com>
2025-11-28 17:10:04 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,315 @@
+
+import 'package:first/workspace.dart';
+import 'package:flutter/material.dart';
+class LoginScreenState extends StatefulWidget {
+  const LoginScreenState({super.key,});
+
+  @override
+  State<LoginScreenState> createState() => _LoginScreenState();
+}
+
+  class _LoginScreenState extends State<LoginScreenState> {
+  bool rememberMe = false;
+  bool obscure = true;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Color(0xFF003E9F),
+//         title: Text(
+//           "Login",
+//           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, // bold text
+//             fontSize: 30,),
+//         ),
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: <Widget>[
+//             Padding(
+//               padding: const EdgeInsets.only(top: 110.0),
+//               child: Center(
+//                 child: Container(
+//                     width: 200,
+//                     height: 100,
+//                     decoration: BoxDecoration(
+//                         // color: Colors.red,
+//                         // border:BoxBorder.all(style: BorderStyle.solid,color: Colors.black,width:5, ),
+//                         borderRadius: BorderRadius.circular(50.0)),
+//                     child: Image.asset('lib/assets/images/logo.png')),
+//               ),
+//             ),
+//             Padding(
+//               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+//               padding: EdgeInsets.fromLTRB(40,20,40,0),
+//               child: TextField(
+//                 decoration: InputDecoration(
+//                     border: OutlineInputBorder(),
+//                     labelText: 'Phone number, email or username',
+//                     hintText: 'Enter valid email id as abc@gmail.com'),
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.only(
+//                   left: 40.0, right: 40.0, top: 20, bottom: 0),
+//               //padding: EdgeInsets.symmetric(horizontal: 15),
+//               child: TextField(
+//                 obscureText: true,
+//                 decoration: InputDecoration(
+//                     border: OutlineInputBorder(),
+//                     labelText: 'Password',
+//                     hintText: 'Enter secure password'),
+//               ),
+//             ),
+//
+//             SizedBox(
+//               height: 65,
+//               width: 330,
+//               child: Container(
+//                 child: Padding(
+//                   padding:  EdgeInsets.only(top: 20.0),
+//                   child: ElevatedButton(
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: Color(0xFF003E9F),
+//                     ),
+//                     child: Text('Log in ',
+//                       style: TextStyle(color: Colors.white, fontSize: 20,),
+//                     ),
+//                     onPressed: () {
+//                       print('Successfully log in ');
+//                       Navigator.push(context, MaterialPageRoute(builder: (_)=>Workspace()));
+//                     },
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             SizedBox(
+//               height: 50,
+//             ),
+//             Container(
+//                 child: Center(
+//                   child: Row(
+//                     children: [
+//                       Padding(
+//                         padding: const EdgeInsets.only(left: 62),
+//                         child: Text('Forgot your login details? '),
+//                       ),
+//
+//                       Padding(
+//                         padding: const EdgeInsets.only(left: 1.0),
+//                         child: InkWell(
+//                             onTap: () {
+//                               print('hello');
+//                             },
+//                             child: Text(
+//                               'Get help logging in.', style: TextStyle(
+//                                 fontSize: 14, color: Colors.blue),)),
+//                       )
+//                     ],
+//                   ),
+//                 )
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0E7CE1), // Top-Bottom gradient look
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.90,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                SizedBox(height: 10),
+
+                // Logo
+                Container(
+                  height: 55,
+                  width: 140,
+                  padding: EdgeInsets.all(4),
+                  child: Image.asset(
+                    "lib/assets/images/logo.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                Text(
+                  "Welcome to LexIq",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+
+                SizedBox(height: 25),
+
+                // Email field
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.email_outlined),
+                      hintText: "Email Id / Advocate Id",
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 15),
+
+                // Password field
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: TextField(
+                    obscureText: obscure,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.lock_outline),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                            obscure ? Icons.visibility_off : Icons.visibility),
+                        onPressed: () {
+                          setState(() => obscure = !obscure);
+                        },
+                      ),
+                      hintText: "Password",
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Login Button
+                Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF00205F), Color(0xFF01A4E9)],
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "LogIn",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 15),
+
+                // Remember me + Forgot Password
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: rememberMe,
+                          activeColor: Colors.blue,
+                          onChanged: (v) {
+                            setState(() => rememberMe = v!);
+                          },
+                        ),
+                        Text("Remember me"),
+                      ],
+                    ),
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                // Divider
+                Row(
+                  children: [
+                    Expanded(child: Divider()),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text("or Sign with"),
+                    ),
+                    Expanded(child: Divider()),
+                  ],
+                ),
+
+                SizedBox(height: 15),
+
+
+                SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account? "),
+                    Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget socialIcon(String url) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 6,
+            color: Colors.black12,
+            spreadRadius: 1,
+            offset: Offset(0, 3),
+          )
+        ],
+      ),
+      child: Image.network(url, height: 32),
+    );
+  }
+}
---
+++ lib/workspace.dart
@@ -0,0 +1,340 @@
+import 'package:flutter/material.dart';
+
+class Workspace extends StatefulWidget {
+  const Workspace({super.key});
+
+  @override
+  State<Workspace> createState() => _WorkspaceState();
+}
+
+class _WorkspaceState extends State<Workspace> {
+  bool isMenuOpen = false; // MENU OPEN / CLOSE STATE
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          dashboardUI(), // MAIN DASHBOARD UI
+
+          // ----------- TAP ANYWHERE TO CLOSE MENU -----------
+          if (isMenuOpen)
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isMenuOpen = false; // CLOSE ON TAP
+                  });
+                },
+                child: Container(
+                  color: Colors.black.withOpacity(0.2), // dim background
+                ),
+              ),
+            ),
+
+          sideMenuUI(), // LEFT MENU
+        ],
+      ),
+    );
+  }
+
+  // ---------------------------------------------------------
+  //                DASHBOARD SCREEN
+  // ---------------------------------------------------------
+  Widget dashboardUI() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+
+            // TOP HEADER
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // LOGO
+                Container(
+                  height: 55,
+                  width: 140,
+                  padding: EdgeInsets.all(4),
+                  child: Image.asset(
+                    "lib/assets/images/logo.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+
+                Row(
+                  children: [
+                    // PROFILE IMAGE
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundImage:
+                      AssetImage("lib/assets/images/profile.png"),
+                    ),
+
+                    const SizedBox(width: 15),
+
+                    // NOTIFICATION
+                    Stack(
+                      children: [
+                        Icon(Icons.notifications_none, size: 32),
+                        Positioned(
+                          right: 0,
+                          top: -2,
+                          child: Container(
+                            padding: EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Text(
+                              "5",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+
+                    const SizedBox(width: 15),
+
+                    // MENU ICON
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isMenuOpen = !isMenuOpen;
+                        });
+                      },
+                      child: Icon(Icons.menu, size: 32),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            SizedBox(height: 30),
+
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    "Good Morning Manjeet!",
+                    style: TextStyle(
+                      color: Color(0xFF004B8D),
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Here's what your Ecourt Services are up to today.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black54, fontSize: 14),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            buildCaseCard(
+              title: "Active Cases",
+              count: "62",
+              badgeText: "Active Case",
+              badgeColor: Color(0xFFBDF8D0),
+              borderColor: Colors.transparent,
+              icon: Icons.edit_document,
+            ),
+
+            SizedBox(height: 20),
+
+            buildCaseCard(
+              title: "Total Hearing",
+              count: "20",
+              badgeText: "Hearings",
+              badgeColor: Color(0xFFF8BBD0),
+              borderColor: Colors.transparent,
+              icon: Icons.description_outlined,
+            ),
+
+            SizedBox(height: 20),
+
+            buildCaseCard(
+              title: "Total Recent Case",
+              count: "15",
+              badgeText: "Recent Cases",
+              badgeColor: Color(0xFFFFECB3),
+              borderColor: Colors.transparent,
+              icon: Icons.file_copy_outlined,
+            ),
+            SizedBox(height: 20),
+
+            buildCaseCard(
+              title: "Total Recent Case",
+              count: "15",
+              badgeText: "Recent Cases",
+              badgeColor: Color(0xFFFFECB3),
+              borderColor: Colors.transparent,
+              icon: Icons.file_copy_outlined,
+            ),
+            SizedBox(height: 20),
+
+            buildCaseCard(
+              title: "Total Recent Case",
+              count: "15",
+              badgeText: "Recent Cases",
+              badgeColor: Color(0xFFFFECB3),
+              borderColor: Colors.transparent,
+              icon: Icons.file_copy_outlined,
+            ),
+            SizedBox(height: 20),
+
+            buildCaseCard(
+              title: "Total Recent Case",
+              count: "15",
+              badgeText: "Recent Cases",
+              badgeColor: Color(0xFFFFECB3),
+              borderColor: Colors.transparent,
+              icon: Icons.file_copy_outlined,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // ---------------------------------------------------------
+  //           LEFT SLIDING SIDE MENU
+  // ---------------------------------------------------------
+  Widget sideMenuUI() {
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+      left: isMenuOpen ? 0 : -290,
+      top: 110,
+      bottom: 0,
+      child: Container(
+        width: 290,
+        color: Color(0xFF0A2F66),
+        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 15),
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(Icons.home, color: Colors.white),
+                SizedBox(width: 10),
+                Text("Dashboard",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 20),
+
+            menuItem(Icons.dashboard, "My Workspace"),
+            menuItem(Icons.travel_explore, "Court Search"),
+            menuItem(Icons.add_box_outlined, "Add New Case"),
+            menuItem(Icons.dashboard_customize, "Display Board"),
+            menuItem(Icons.calendar_month, "Calendar"),
+            menuItem(Icons.note_alt_outlined, "Report"),
+            menuItem(Icons.groups, "Team Collaboration"),
+            menuItem(Icons.receipt_long, "Create Invoice"),
+            menuItem(Icons.payments_outlined, "Billing & Invoice"),
+            menuItem(Icons.settings, "Setting"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // MENU ITEM WIDGET
+  Widget menuItem(IconData icon, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white70, size: 22),
+          SizedBox(width: 12),
+          Text(title, style: TextStyle(color: Colors.white, fontSize: 15)),
+        ],
+      ),
+    );
+  }
+
+  // ---------------- CASE CARD ----------------
+  Widget buildCaseCard({
+    required String title,
+    required String count,
+    required String badgeText,
+    required Color badgeColor,
+    required Color borderColor,
+    required IconData icon,
+    bool isHighlighted = false,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(18),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(
+          color: borderColor,
+          width: isHighlighted ? 2 : 1,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(icon, size: 30, color: Colors.black87),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                decoration: BoxDecoration(
+                  color: badgeColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  badgeText,
+                  style: TextStyle(
+                      fontSize: 12, fontWeight: FontWeight.w600),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 12),
+          Text(
+            title,
+            style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15,
+                fontWeight: FontWeight.w600),
+          ),
+          SizedBox(height: 5),
+          Text(
+            count,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
